52abe945d616654053693e7ffd4c6c0cce852dde
fadeomar <fadeomar2015@gmail.com>
2023-01-24 21:50:54 +0200

    increase on hover

--- src/components/DrilldownPie/Arc.js
+++ src/components/DrilldownPie/Arc.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import * as d3 from "d3";
 import styled from "styled-components";
 
@@ -8,9 +9,25 @@ const Path = styled.path`
 `;
 
 const Arc = ({ arcData }) => {
-  const arc = d3.arc().innerRadius(5).outerRadius(75);
-
-  return <Path d={arc(arcData)} index={arcData.data.index} />;
+  const [r, setR] = useState(0);
+  const arc = d3
+    .arc()
+    .innerRadius(15 + r)
+    .outerRadius(105 + r);
+  const onMouseOver = () => {
+    setR(20);
+  };
+  const onMouseOut = () => {
+    setR(0);
+  };
+  return (
+    <Path
+      d={arc(arcData)}
+      index={arcData.data.index}
+      onMouseOver={onMouseOver}
+      onMouseOut={onMouseOut}
+    />
+  );
 };
 
 export default Arc;
